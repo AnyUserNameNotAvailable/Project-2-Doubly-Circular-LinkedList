0182de08f470a0e0fc9f7c7abb8407c7af468d13
AnyUserNameNotAvailable <113045469+AnyUserNameNotAvailable@users.noreply.github.com>
2023-10-18 16:07:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,37 @@
 #include "list.hpp"
 
 int main() {
-    List::appendNode(11);
-    List::appendNode(22);
-    List::appendNode(33);
-    List::appendNode(44);
-    List::appendNode(55);
+    List myList;
 
-    List::displayList();
+    myList.appendNode(11);
+    myList.appendNode(22);
+    myList.appendNode(33);
+    myList.appendNode(44);
+    myList.appendNode(55);
 
+    myList.displayList();
+
+    List::Node* nodePtr = myList.head;
+    if (nodePtr) {
+        std::cout << "Node\tValue\tNext\tPrevious" << std::endl;
+        int nodeNumber = 1;
+        do {
+            std::cout << nodeNumber << "\t" << nodePtr->data << "\t";
+
+            if (nodePtr->next)
+                std::cout << nodePtr->next->data << "\t";
+            else
+                std::cout << "N/A\t";
+
+            if (nodePtr->prev)
+                std::cout << nodePtr->prev->data << std::endl;
+            else
+                std::cout << "N/A" << std::endl;
+
+            nodePtr = nodePtr->next;
+            nodeNumber++;
+        } while (nodePtr != myList.head);
+    }
+
+    return 0;
 }
